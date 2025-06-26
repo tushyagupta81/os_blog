af9b96ed9831b2c8122af63c06a58ae7bfcf1e59
Tushya Gupta <tushyagupta81@gmail.com>
2025-06-26 20:51:41 +0530

    writting to the VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,18 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello world!";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
     loop {}
 }
-
