1a50cba514924ef62199809a1815f2349f2bcf86
Tushya Gupta <tushyagupta81@gmail.com>
2025-06-29 17:06:51 +0530

    catching faults

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,14 @@ use os_blog::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    os_blog::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It didn't crash");
     loop {}
 }
 
